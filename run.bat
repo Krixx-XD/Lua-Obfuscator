07e108e9a6a34875a9cbe7195bcba6e786a67413
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2024-11-09 08:01:59 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@ type Script.lua > Ironbrew/netcoreapp2.0/input.txt
 cd Ironbrew/netcoreapp2.0/
 dotnet "IronBrew2 CLI.dll" "input.txt"
 cd ../..
-luac Ironbrew/netcoreapp2.0/out.lua
-lua Main.lua
+luac52 Ironbrew/netcoreapp2.0/out.lua
+lua52 Main.lua
 pause
